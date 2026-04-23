46bd70d04368ccdc2ea2caf4673632ffdc57d0de
Evan Stachowiak <evanstachowiak@users.noreply.github.com>
2026-04-23 21:58:27 +0200

    Add material theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,31 +1,51 @@
 return {
-  'folke/tokyonight.nvim',
+'marko-cerovac/material.nvim',
   lazy = false,
   priority = 1000,
   opts = {},
   config = function()
-    require('tokyonight').setup {
-      on_highlights = function(hl, colors)
-        -- hl.IncSearch = { bg = colors.bg_dark, fg = colors.black }
-        hl.IncSearch = {
-          -- bg = colors.blue,
-          fg = colors.blue,
-        }
-        -- hl.Search = {
-        --   bg = colors.blue,
-        --   fg = colors.bg_dark,
-        -- }
-        -- hl.LineNr = {
-        --   fg = colors.cyan,
-        -- }
-        hl.CursorLineNr = {
-          fg = colors.cyan,
-        }
-      end,
-    }
-    vim.cmd 'colorscheme tokyonight-night'
+    require('material.functions').change_style("palenight")
   end,
 }
+
+-- return {
+--   'folke/tokyonight.nvim',
+--   lazy = false,
+--   priority = 1000,
+--   opts = {},
+--   config = function()
+--     require('tokyonight').setup {
+--       on_highlights = function(hl, colors)
+--         -- hl.IncSearch = { bg = colors.bg_dark, fg = colors.black }
+--         hl.IncSearch = {
+--           -- bg = colors.blue,
+--           fg = colors.blue,
+--         }
+--         -- hl.Search = {
+--         --   bg = colors.blue,
+--         --   fg = colors.bg_dark,
+--         -- }
+--         -- hl.LineNr = {
+--         --   fg = colors.cyan,
+--         -- }
+--         hl.CursorLineNr = {
+--           fg = colors.cyan,
+--         }
+--       end,
+--     }
+--     vim.cmd 'colorscheme tokyonight-night'
+--   end,
+-- }
+
+-- return {
+--   'doums/darcula',
+--   lazy = false,
+--   priority = 1000,
+--   opts = {},
+--   config = function()
+--     vim.cmd 'colorscheme darcula'
+--   end,
+-- }
 
 -- return {
 --   'rebelot/kanagawa.nvim',
