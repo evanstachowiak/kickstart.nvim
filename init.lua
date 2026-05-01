9898c48c6a1505f82b3603c76630717e57a2f527
Evan Stachowiak <evanstachowiak@users.noreply.github.com>
2026-05-01 10:15:30 +0200

    Add codediff plugin

--- init.lua
+++ init.lua
@@ -257,6 +257,7 @@ rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   { import = 'plugins' },
+  { import = 'plugins/themes' },
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
 
